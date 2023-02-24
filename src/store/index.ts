58e530cfb22f0d4d87d30d5497181f67cf122d5d
CQUAN <cuuquan2703@gmail.com>
2023-02-24 20:57:39 +0700

    refactor: config router,redux, tailwind

---
+++ src/store/index.ts
@@ -0,0 +1,30 @@
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  ThunkAction,
+} from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux/es/exports";
+import { TypedUseSelectorHook } from "react-redux/es/types";
+import { countReducer, COUNT_REDUCER } from "./countReducer";
+
+export const rootReducer = combineReducers({
+  [COUNT_REDUCER]: countReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export default store;
