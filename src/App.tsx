cbc47aaa8742cdf816d9940277937a36ac0dad19
CQUAN <cuuquan2703@gmail.com>
2023-02-25 09:31:41 +0700

    chore: test redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useAppDispatch, useAppSelector } from "./store";
+import { countAction } from "./store/countReducer";
 
 function App() {
-  return <div className="text-5xl text-black underline">test</div>;
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.count.count);
+
+  const increase = () => {
+    dispatch(countAction.increase());
+  };
+  const decrease = () => {
+    dispatch(countAction.decrease());
+  };
+
+  return (
+    <>
+      <button className="mr-10" onClick={increase}>
+        increase
+      </button>
+      <button onClick={decrease}>decrease</button>
+      <div className="text-5xl text-black underline">{count}</div>
+    </>
+  );
 }
 
 export default App;
